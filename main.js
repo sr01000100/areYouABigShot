e9da2e71b1e378d7755b1fa71c1cd02ecad4ee5b
Bardock1955 <dbecker@beckerzone.com>
2025-08-10 01:08:25 -0400

    phone version and more

--- main.js
+++ main.js
@@ -7,16 +7,29 @@ button.addEventListener("click",function(){
     var num=Math.floor(Math.random()*101)
     //document.getElementById("numb").innerHTML=num
     //var num=69
-    if(num==69||num==67){
+    console.log(num);
+    if(num==5||num==13||num==21||num==67||num==69){
         document.getElementById("resp").innerHTML="Yes"
         image.removeAttribute("hidden")
+        sound.currentTime=0;
         sound.play();
+        window.alert("[BIG SHOT]");
+        button.disabled=true;
     }else{
         document.getElementById("resp").innerHTML="No"
         image.setAttribute("hidden","hidden")
-        document.close();
+        sound.pause();
+        sound.currentTime=0;
     }
 
 
     
 });
+
+sound.onended=function(){
+    button.disabled=false;
+    image.setAttribute("hidden","hidden")
+    document.getElementById("resp").innerHTML="No more [BIG SHOT]"
+    sound.pause();
+    sound.currentTime=0;
+}
